6264a5ddb524c5c6a601c10eaeee56b63408f8a4
Mohamed Shosha <moshosha267@gmail.com>
2025-03-16 15:00:26 +0200

    feat: Add search functionality for products by name with pagination

--- src/main/java/com/shosha/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/shosha/ecommerce/config/MyDataRestConfig.java
@@ -15,11 +15,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * The type My data rest config.
+ */
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
 
+    /**
+     * Instantiates a new My data rest config.
+     *
+     * @param theEntityManager the entity manager
+     */
     @Autowired
     public MyDataRestConfig(EntityManager theEntityManager) {
         entityManager = theEntityManager;
@@ -27,45 +35,34 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-
         HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
 
-        // disable HTTP methods for Product: PUT, POST, DELETE and PATCH
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .withItemExposure((metdata, httpMethods)
+                        -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods)
+                        -> httpMethods.disable(theUnsupportedActions));
 
-        // disable HTTP methods for ProductCategory: PUT, POST, DELETE and PATCH
         config.getExposureConfiguration()
                 .forDomainType(ProductCategory.class)
-                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+                .withItemExposure((metdata, httpMethods)
+                        -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods)
+                        -> httpMethods.disable(theUnsupportedActions));
 
-        // call an internal helper method
         exposeIds(config);
-
     }
 
     private void exposeIds(RepositoryRestConfiguration config) {
-
-        // expose entity ids
-        //
-
-        // - get a list of all entity classes from the entity manager
         Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
-
-        // - create an array of the entity types
         List<Class> entityClasses = new ArrayList<>();
 
-        // - get the entity types for the entities
         for (EntityType tempEntityType : entities) {
             entityClasses.add(tempEntityType.getJavaType());
         }
 
-        // - expose the entity ids for the array of entity/domain types
         Class[] domainTypes = entityClasses.toArray(new Class[0]);
         config.exposeIdsFor(domainTypes);
     }
-
 }
