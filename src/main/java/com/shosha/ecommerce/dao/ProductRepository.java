60347cfe0b5646fb0a3784eeb22621322218b4e9
Mohamed Shosha <moshosha267@gmail.com>
2025-04-12 22:47:43 +0200

    Refactor search functionality by adding APIs to simplify and improve search complexity

--- src/main/java/com/shosha/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/shosha/ecommerce/dao/ProductRepository.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+/*
+http://localhost:8080/api/products/search/findByCategoryId?id=1&page=0&size=5
+GET http://localhost:8080/api/products/search/findByNameContaining?name=Crash%20Course%20in%20Python&page=0&size=5
+ */
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
     Page<Product> findByNameContaining(@Param("name") String name, Pageable pageable);
